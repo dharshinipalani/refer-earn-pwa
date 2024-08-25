053179fa60e1202edae6ea46e781046088d87681
dharshinipalani <drishadharshini1@gmail.com>
2024-08-25 17:04:07 +0530

    dharshu | header, banner, refer

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, Button, Typography } from "@mui/material";
+import Header from "./Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+      <Header />
+      <img
+        src="/images/banner.png"
+        alt="refer a friend"
+        width="100%"
+        height="30%"
+      />
+      <Button variant="contained" color="secondary" size="large">
+        <Typography color="primary">refer</Typography>
+      </Button>
+    </Box>
   );
 }
 
---
+++ src/Theme.tsx
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ec7162",
+    },
+    secondary: {
+      main: "#ffffff",
+    },
+  },
+});
+
+export default theme;
